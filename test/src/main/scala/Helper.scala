c25b4477adc1564bff6a11313e40630ae6efa31f
ketan purohit <ketanpurohit0@gmail.com>
2020-10-31 16:14:14 +0000

    Added a first cut sparksesion. Fails.

--- test/src/main/scala/Helper.scala
+++ test/src/main/scala/Helper.scala
@@ -1,8 +1,20 @@
 package com.kkp.Unt
+import org.apache.spark.{SparkConf,SparkContext}
+import org.apache.spark.sql.SparkSession
 class Helper {
 
   def printMe(m :String) : Unit = {
     println(m)
+  }
+
+  def getSparkSession(sparkMaster : String, appName : String) : SparkSession = {
+    val conf = new SparkConf().setMaster(sparkMaster).set("spark.app.name", appName)
+    val sc = new SparkContext(conf)
+    val spark = SparkSession.builder().getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+    spark.conf.set("spark.sql.crossJoin.enabled", true)
+    spark.conf.set("spark.sql.caseSensitive", false)
+    return spark
   }
 
 }
--- test/src/main/scala/Main.scala
+++ test/src/main/scala/Main.scala
@@ -4,6 +4,8 @@ object Main {
 def main(args: Array[String]): Unit = {
   var x = new Helper()
   x.printMe("Hello World")
+  val spark = x.getSparkSession("local", "test")
+  import spark.implicits._
 }
 
 }
