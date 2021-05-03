161259f7589d8f728a1b9ede10dd00ac392269db
ketan purohit <ketanpurohit0@gmail.com>
2021-05-03 12:18:57 +0100

    Rename State to MyState

--- books/fps/src/test/scala/ch85.scala
+++ books/fps/src/test/scala/ch85.scala
@@ -2,17 +2,17 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class ch85 extends AnyFunSuite {
 
-  case class State[S,A](run: S => (S,A)) {
-
-    def flatMap[B](f : A => State[S,B]) : State[S,B] = State {
+  case class MyState[S,A](run: S => (S,A)) {
+    
+    def flatMap[B](f : A => MyState[S,B]) : MyState[S,B] = MyState {
         (s0: S) => {
           val (s1, a1) = run(s0)
           val s2 = f(a1)
           s2.run(s1)
         }
     }
-
-    def map[B](f: A => B) : State[S,B] = State {
+    
+    def map[B](f: A => B) : MyState[S,B] = MyState {
       (s0: S) => {
         val (s1, a1) = run(s0)
         val b1 = f(a1)
@@ -20,17 +20,17 @@ class ch85 extends AnyFunSuite {
       }
     }
   }
-
+  
   case class GolfState(distance: Int)
 
-  def nextStroke(distance: Int) : State[GolfState, Int] = State {
+  def nextStroke(distance: Int) : MyState[GolfState, Int] = MyState {
     (s: GolfState) => {
       val newValue = s.distance + distance
       (GolfState(newValue), newValue)
     }
   }
 
-  test("my.State") {
+  test("my.MyState") {
     val initialState = GolfState(0)
     val state = for {
       _ <- nextStroke(20)
