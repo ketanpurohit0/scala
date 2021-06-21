94b239466f347dff4e5970370d88c98106d3e34c
ketan purohit <ketanpurohit0@gmail.com>
2021-06-21 15:55:12 +0100

    Need to use scala 3.

---
+++ rtvjm/src/main/scala/com/rockthejvm/ContextualAbstractions.scala
@@ -0,0 +1,21 @@
+package main.scala.com.rockthejvm
+
+object ContextualAbstractions {
+// TODO - Will required scala 3
+  
+//  val aList = List(2,1,3,4)
+//  val anOrderedList = aList.sorted
+//  given descendingOrder: Ordering[Int] = Ordering.fromLessThan(_ > _)
+//  trait Combinator[A] {
+//    def combine(x: A, y: A): A
+//  }
+//
+//  def combineAll[A](list: List[A])(using combinator: Combinator[A]): A =
+//    list.reduce((a,b) => combinator.combine(a,b))
+//
+//  val theSum = combineAll(aList)
+//
+//  def main(args: Array[String]): Unit = {
+//    println(anOrderedList)
+//  }
+}
