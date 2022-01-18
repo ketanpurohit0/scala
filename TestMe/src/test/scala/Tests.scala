37a0b749e9ecbf252e6bb794dda5b2de82da4cba
ketan purohit <ketanpurohit0@gmail.com>
2022-01-18 09:38:00 +0000

    First attempt at understanding json differences.

--- TestMe/src/test/scala/Tests.scala
+++ TestMe/src/test/scala/Tests.scala
@@ -1,5 +1,5 @@
 import org.scalatest.funsuite.AnyFunSuite
-import org.apache.spark.sql.functions.{col, explode, get_json_object, lit, schema_of_json, to_json}
+import org.apache.spark.sql.functions.{col, explode, from_json, get_json_object, lit, schema_of_json, to_json}
 import org.apache.spark.sql.types.{DataType, StructType}
 class Tests extends AnyFunSuite{
 
@@ -34,20 +34,55 @@ class Tests extends AnyFunSuite{
 //    |  1|   29304|         2|{'seqNum': 2, 'ma...|
 //    +---+--------+----------+--------------------+
 
-
       }
 
-  test("CleanAndFlatten") {
+  test("ConvertStringColToJson") {
+    val m = Map[String,String]()
     val df = Helper.loadCSV(spark, resourceCsvPath)
-    val jsonSample = df.select("match_element").collect()
+    val jsonSample = df.filter(col("match_element").contains("MatchStatusUpdate")).select("match_element").collect()
     val sampleJson = jsonSample(0).getString(0)
     val schemaJson = spark.range(1)
       .select(schema_of_json(lit(sampleJson)))
-      .collect()(0)(0)
+      .collect()(0).getString(0)
 
-    df.select(get_json_object(col("match_element"), "*")).show(5)
+//    print(schemaJson)
+//    struct<eventElementType:string,matchStatus:struct<courtNum:bigint,firstServer:string,matchState:struct<locationTimestamp:string,state:string>,numSets:bigint,scoringType:string,teamAPlayer1:string,teamAPlayersDetails:struct<player1Country:string,player1Id:string>,teamBPlayer1:string,teamBPlayersDetails:struct<player1Country:string,player1Id:string>,tieBreakType:string,tossChooser:string,tossWinner:string,umpire:string,umpireCode:string,umpireCountry:string>,matchTime:string,seqNum:bigint,timestamp:string>
 
-    df.select(from)
+    val df2 = df.withColumn("structFromJson", from_json(col("match_element"), schemaJson, m))
+//    df2.printSchema()
+//    |-- _c0: string (nullable = true)
+//    |-- match_id: string (nullable = true)
+//    |-- message_id: string (nullable = true)
+//    |-- match_element: string (nullable = true)
+//    |-- structFromJson: struct (nullable = true)
+//    |    |-- eventElementType: string (nullable = true)
+//    |    |-- matchStatus: struct (nullable = true)
+//    |    |    |-- courtNum: long (nullable = true)
+//    |    |    |-- firstServer: string (nullable = true)
+//    |    |    |-- matchState: struct (nullable = true)
+//    |    |    |    |-- locationTimestamp: string (nullable = true)
+//    |    |    |    |-- state: string (nullable = true)
+//    |    |    |-- numSets: long (nullable = true)
+//    |    |    |-- scoringType: string (nullable = true)
+//    |    |    |-- teamAPlayer1: string (nullable = true)
+//    |    |    |-- teamAPlayersDetails: struct (nullable = true)
+//    |    |    |    |-- player1Country: string (nullable = true)
+//    |    |    |    |-- player1Id: string (nullable = true)
+//    |    |    |-- teamBPlayer1: string (nullable = true)
+//    |    |    |-- teamBPlayersDetails: struct (nullable = true)
+//    |    |    |    |-- player1Country: string (nullable = true)
+//    |    |    |    |-- player1Id: string (nullable = true)
+//    |    |    |-- tieBreakType: string (nullable = true)
+//    |    |    |-- tossChooser: string (nullable = true)
+//    |    |    |-- tossWinner: string (nullable = true)
+//    |    |    |-- umpire: string (nullable = true)
+//    |    |    |-- umpireCode: string (nullable = true)
+//    |    |    |-- umpireCountry: string (nullable = true)
+//    |    |-- matchTime: string (nullable = true)
+//    |    |-- seqNum: long (nullable = true)
+//    |    |-- timestamp: string (nullable = true)
+    df2.select("structFromJson.eventElementType", "structFromJson.matchStatus.matchState.state").show()
+    df2.select("structFromJson.*").show()
   }
 
 
