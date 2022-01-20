beb21f579e247abd89e7c814eb17ab86bc78e578
ketan purohit <ketanpurohit0@gmail.com>
2022-01-20 14:03:57 +0000

    Flatten working (1st cut)

--- TestMe/src/main/scala/Helper.scala
+++ TestMe/src/main/scala/Helper.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.sql.functions.{col, lit, schema_of_json}
+import org.apache.spark.sql.types.{StructField, StructType}
 import org.apache.spark.{SparkConf, SparkContext}
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.{Column, DataFrame, SparkSession}
 
 object Helper {
   def getSparkSession(sparkMaster: String, appName: String): SparkSession = {
@@ -17,6 +18,8 @@ object Helper {
   def loadCSV(spark: SparkSession, filePath: String): DataFrame = {
     spark.read
       .option("header", "true")
+      .option("multiline","true")
+      .option("escape", "\"")
       .csv(filePath)
   }
 
@@ -30,4 +33,31 @@ object Helper {
       .collect()(0).getString(0)
     schemaJson
   }
+
+  def getJsonSchema(spark: SparkSession, df: DataFrame, jsonColName: String): StructType = {
+    import spark.implicits._
+    spark.read.json(df.select(col(jsonColName).as[String])).schema
+  }
+
+  def flattenSchema(schema: StructType, parent: Option[String]) : Array[String] = {
+    // heavily inspired by https://sparkbyexamples.com/spark/spark-flatten-nested-struct-column/
+    val columnTag = parent match {
+      case Some(parentValue) => s"${parentValue}."
+      case None => ""
+    }
+
+    val cols = schema.fields.flatMap(f => {
+        f.dataType match {
+          case s : StructType => flattenSchema(s, Some(s"${columnTag}${f.name}"))//s"Struct<${f.name}>"
+          case _ => Array(s"${columnTag}${f.name}")
+        }
+    })
+
+
+    cols
+  }
+
+
+
+
 }
