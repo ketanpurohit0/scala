a8fed8efb5aea8873a66734eaa8184ee30a52f08
ketan purohit <ketanpurohit0@gmail.com>
2022-01-25 14:53:50 +0000

    Using ScalaCheck

--- TeralyticsTest/build.sbt
+++ TeralyticsTest/build.sbt
@@ -6,6 +6,8 @@ scalaVersion := "2.12.13"
 
 libraryDependencies += "org.scalactic" % "scalactic_2.12" % "3.2.5"
 libraryDependencies += "org.scalatest" % "scalatest_2.12" % "3.2.5"
+libraryDependencies += "org.scalacheck" %% "scalacheck" % "1.15.4" % Test
+
 libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.9.4"
 // Presence of this line causes exception at start time
 //libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.3.0-alpha12"
--- TeralyticsTest/src/test/scala/Tests.scala
+++ TeralyticsTest/src/test/scala/Tests.scala
@@ -1,5 +1,44 @@
+import org.scalacheck.{Prop, Properties}
+import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.should.Matchers.{convertToAnyShouldWrapper, equal}
+import org.scalatest.prop.Configuration.minSuccessful
 
-class Tests extends AnyFunSuite{
+object Tests extends Properties("Examples") {
+  implicit val arbitraryInts: Gen[Int] = Arbitrary.arbitrary[Int]
+  implicit val nonZeroInts: Gen[Int] = Arbitrary.arbitrary[Int] suchThat (_ != 0)
+  implicit val positiveInts: Gen[Int] = Arbitrary.arbitrary[Int] suchThat (_ > 0)
+
+  property("n") = Prop.forAll({ i: Int => i == i })
+
+  property("list tail") = Prop.forAll { (n: Int, l: List[Int]) =>
+    (n :: l).tail == l
+  }
+
+  property("n+m") = Prop.forAll { (n: Int, m: Int) =>
+    n + m == m + n
+  }
+
+  case class Person(
+      firstName: String,
+      lastName: String,
+      age: Int
+  ) {
+    def isTeenager = age >= 13 && age <= 19
+  }
+
+  val genPerson = {
+    import org.scalacheck.Gen.{choose, oneOf}
+    for {
+      firstName <- oneOf("Alan", "Ada", "Alonzo")
+      lastName <- oneOf("Lovelace", "Turing", "Church")
+      age <- choose(1, 100)
+    } yield Person(firstName, lastName, age)
+  }
+  implicit val carbPerson: Arbitrary[Person] = Arbitrary(genPerson)
+
+  property("ex1") = Prop.forAll { (p: Person) =>
+    p.isTeenager == (p.age >= 13 && p.age <= 19)
+  }
 
 }
