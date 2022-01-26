e6e3aaaa41418679b30e1d8cb53012e329b618bf
ketan purohit <ketanpurohit0@gmail.com>
2022-01-26 14:46:20 +0000

    Working to understand ScalaCheck

--- TeralyticsTest/src/test/scala/Tests.scala
+++ TeralyticsTest/src/test/scala/Tests.scala
@@ -1,21 +1,23 @@
 import org.scalacheck.{Prop, Properties}
 import org.scalacheck.{Arbitrary, Gen}
-import org.scalatest.funsuite.AnyFunSuite
-import org.scalatest.matchers.should.Matchers.{convertToAnyShouldWrapper, equal}
-import org.scalatest.prop.Configuration.minSuccessful
 
 object Tests extends Properties("Examples") {
   implicit val arbitraryInts: Gen[Int] = Arbitrary.arbitrary[Int]
   implicit val nonZeroInts: Gen[Int] = Arbitrary.arbitrary[Int] suchThat (_ != 0)
   implicit val positiveInts: Gen[Int] = Arbitrary.arbitrary[Int] suchThat (_ > 0)
+  implicit val first10Ints: Gen[Int] = positiveInts.suchThat(_ <= 9)
 
-  property("n") = Prop.forAll({ i: Int => i == i })
+  property("index") = Prop.forAll({ a: Array[Int] =>
+    a.sorted.distinct.length == p.delete_duplicates(a.sorted)
+  })
+
+  property("n") = Prop.forAll(positiveInts)({ i: Int => i == i })
 
   property("list tail") = Prop.forAll { (n: Int, l: List[Int]) =>
     (n :: l).tail == l
   }
 
-  property("n+m") = Prop.forAll { (n: Int, m: Int) =>
+  property("n+m") = Prop.forAll(arbitraryInts, nonZeroInts) { (n: Int, m: Int) =>
     n + m == m + n
   }
 
@@ -24,10 +26,10 @@ object Tests extends Properties("Examples") {
       lastName: String,
       age: Int
   ) {
-    def isTeenager = age >= 13 && age <= 19
+    def isTeenager: Boolean = age >= 13 && age <= 19
   }
 
-  val genPerson = {
+  val genPerson: Gen[Person] = {
     import org.scalacheck.Gen.{choose, oneOf}
     for {
       firstName <- oneOf("Alan", "Ada", "Alonzo")
@@ -39,6 +41,7 @@ object Tests extends Properties("Examples") {
 
   property("ex1") = Prop.forAll { (p: Person) =>
     p.isTeenager == (p.age >= 13 && p.age <= 19)
+
   }
 
 }
--- TeralyticsTest/src/test/scala/Tests2.scala
+++ TeralyticsTest/src/test/scala/Tests2.scala
@@ -1,6 +1,35 @@
 import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers.{be, equal}
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
 
 class Tests2 extends AnyFunSuite {
 
-  test("Tests2") {}
+  test("Tests2") {
+
+    val inputs = Seq(
+      Array(1, 2, 2, 3, 3),
+      Array(1, 4, 5, 6, 4, 3, 2, 4, 2, 4, 5).sorted
+    )
+
+    inputs.foreach(i => { println(p.delete_duplicates(i)); println(i.mkString(",")) })
+  }
+}
+
+object p {
+  def delete_duplicates(A: Array[Int]): Int = {
+    if (A.length == 0) {
+      return 0
+    }
+    var write_index = 1
+    (1 until (A.length)).foreach { i =>
+      if (A(write_index - 1) != A(i)) {
+        A(write_index) = A(i)
+        write_index += 1
+      }
+
+    }
+
+    return write_index
+
+  }
 }
