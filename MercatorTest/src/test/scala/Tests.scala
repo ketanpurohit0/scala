59e9489434e603dec16ff59fadea62ad1fc9462f
ketan purohit <ketanpurohit0@gmail.com>
2022-02-22 10:51:46 +0000

    Step 0 : Project setup - Adding test classes

---
+++ MercatorTest/src/test/scala/Tests.scala
@@ -0,0 +1,10 @@
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.matchers.must.Matchers.{be, equal}
+import org.scalatest.matchers.should.Matchers.convertToAnyShouldWrapper
+
+class Tests extends AnyFunSuite {
+
+  test("test1") {}
+
+  test("test2") {}
+}
